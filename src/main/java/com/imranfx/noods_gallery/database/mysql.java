480f833d547525c15f878f1532c2f3b355b4e25a
mohdmeran <blackchoco3@gmail.com>
2021-11-26 13:59:25 +0800

    update mysql table name into generic

--- src/main/java/com/imranfx/noods_gallery/database/mysql.java
+++ src/main/java/com/imranfx/noods_gallery/database/mysql.java
@@ -20,6 +20,7 @@ public class mysql {
     String url;
     String user;
     String pass;
+    String tableName;
     Connection connection;
     
     public mysql() {
@@ -32,6 +33,7 @@ public class mysql {
         this.url = dotenv.get("env_url");
         this.user = dotenv.get("env_user");
         this.pass = dotenv.get("env_pass");
+        this.tableName = dotenv.get("env_tableName");
         this.connect();
     }
     
@@ -68,17 +70,17 @@ public class mysql {
     }
     
     public ResultSet search_index(String q) {
-        String query = "select * from image where (name like '%" + q + "%') or (description like '%" + q + "%')";
+        String query = "select * from " + tableName + " where (name like '%" + q + "%') or (description like '%" + q + "%')";
         return query_result(query);
     }
     
     public ResultSet get_all_image(){
-        String query = "select * from image";
+        String query = "select * from " + tableName;
         return query_result(query);
     }
     
     public void add_image(String name, String description) {
-        String q = String.format("insert into image(name, description) values ('%s', '%s');", name, description);
+        String q = String.format("insert into %s(name, description) values ('%s', '%s');", tableName, name, description);
         this.query(q);
     }
     
@@ -87,18 +89,18 @@ public class mysql {
     }
     
     public void update_description(String imgName, String desc) {
-        String q = String.format("update image set description = '%s' where name = '%s'", desc, imgName);
+        String q = String.format("update %s set description = '%s' where name = '%s'", tableName, desc, imgName);
         this.query(q);
     }
     
     public void delete_image(String name) {
-        String q = String.format("delete from image where (name like '%s')", name);
+        String q = String.format("delete from %s where (name like '%s')", tableName, name);
         this.query(q);
     }
     
     public boolean isEmpty() {
         try {
-            String q = String.format("select count(name) as total from image");
+            String q = String.format("select count(name) as total from %s", tableName);
             
             ResultSet r = this.query_result(q);
             
